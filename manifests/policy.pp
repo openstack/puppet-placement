0b46d52f4ed31b13ed05da82284df633b547c15d
Thomas Goirand <zigo@debian.org>
2021-04-01 23:26:14 +0200

    Allow to configure policy_dirs
    This patch makes it possible to override the current service default,
    which is /etc/<service>/policy.d .
    
    Change-Id: I88f14cfc1ce192c38080b1a4a07e83061d276067

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -32,11 +32,16 @@
 #   (Optional) Path to the placement policy.yaml file
 #   Defaults to /etc/placement/policy.yaml
 #
+# [*policy_dirs*]
+#   (Optional) Path to the placement policy folder
+#   Defaults to $::os_service_default
+#
 class placement::policy (
   $enforce_scope        = $::os_service_default,
   $enforce_new_defaults = $::os_service_default,
   $policies             = {},
   $policy_path          = '/etc/placement/policy.yaml',
+  $policy_dirs          = $::os_service_default,
 ) {
 
   include placement::deps
@@ -56,7 +61,8 @@ class placement::policy (
   oslo::policy { 'placement_config':
     enforce_scope        => $enforce_scope,
     enforce_new_defaults => $enforce_new_defaults,
-    policy_file          => $policy_path
+    policy_file          => $policy_path,
+    policy_dirs          => $policy_dirs,
   }
 
 }
