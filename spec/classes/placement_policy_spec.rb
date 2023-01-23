cede98893ae8f0fcfea775624a4087a3defef121
Takashi Kajinami <tkajinam@redhat.com>
2023-01-23 14:40:33 +0900

    Expose policy_default_rule
    The option has been managed by the underlying puppet-oslo module but
    has not been configurable. This introduces the parameter to customize
    the option.
    
    Change-Id: Ic0d3b20bdf63599160bd8419d6e3b13b73d09cea

--- spec/classes/placement_policy_spec.rb
+++ spec/classes/placement_policy_spec.rb
@@ -9,6 +9,7 @@ describe 'placement::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_path          => '/etc/placement/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/placement/policy.d',
           :policies             => {
             'context_is_admin' => {
@@ -37,6 +38,7 @@ describe 'placement::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_file          => '/etc/placement/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/placement/policy.d',
         )
       end
