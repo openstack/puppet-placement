bba3585dbd9701698f8200973e32feafc8c2c128
Tobias Urdin <tobias.urdin@binero.com>
2023-06-25 23:57:58 +0200

    Add per module policy service refresh
    Updating the policies for this project should only
    refresh the services that reads it.
    
    Change-Id: Iaf0bae66224d568a2a05f4ac5281c36bde2705e0
    (cherry picked from commit 11e694b010b26dd1b9065f52b759366dc24ca7b8)
    (cherry picked from commit 75ed4dba31af3712630a807dcd581a6eb8a90375)
    (cherry picked from commit 14cc9ef3d58d770042c463594f9b7d423edee2c5)

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -41,7 +41,7 @@ class placement::deps {
 
   # policy config should occur in the config block also.
   Anchor['placement::config::begin']
-  -> Openstacklib::Policy<||>
+  -> Openstacklib::Policy<| tag == 'placement' |>
   ~> Anchor['placement::config::end']
 
   # On any uwsgi config change, we must restart Placement.
