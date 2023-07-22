cb9d3c9f1f2c9a704469cc7293b1e3c2483092c0
Takashi Kajinami <tkajinam@redhat.com>
2023-07-22 23:51:58 +0900

    Ensure purge_config takes a boolean value
    The purge_config parameters only accept boolean values. This enforces
    that using the typed parameters.
    
    Change-Id: I637f94a04511256c0d69284522d7ba73a0332c5d

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -52,7 +52,7 @@ class placement::policy (
   $policy_path          = '/etc/placement/policy.yaml',
   $policy_default_rule  = $facts['os_service_default'],
   $policy_dirs          = $facts['os_service_default'],
-  $purge_config         = false,
+  Boolean $purge_config = false,
 ) {
 
   include placement::deps
