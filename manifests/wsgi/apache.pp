f0379d4a633202c0e69009a10f6c833dcc87c5d5
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2023-11-14 16:35:44 +0100

    allow to use access_log_env_var for apache wsgi
    Change-Id: I68ad0c86d9b5f05a9fa9c95ee00a52e0135bae9e
    Signed-off-by: Benedikt Trefzer <benedikt.trefzer@cirrax.com>

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -85,6 +85,9 @@
 #   (Optional) The log format for the virtualhost.
 #   Defaults to undef.
 #
+# [*access_log_env_var*]
+#   (Optional) Specifies that only requests with particular environment variables be logged.
+#
 # [*error_log_file*]
 #   (Optional) The error log file name for the virtualhost.
 #   Defaults to undef.
@@ -150,6 +153,7 @@ class placement::wsgi::apache (
   $access_log_pipe             = undef,
   $access_log_syslog           = undef,
   $access_log_format           = undef,
+  $access_log_env_var          = undef,
   $error_log_file              = undef,
   $error_log_pipe              = undef,
   $error_log_syslog            = undef,
@@ -195,6 +199,7 @@ class placement::wsgi::apache (
     access_log_pipe             => $access_log_pipe,
     access_log_syslog           => $access_log_syslog,
     access_log_format           => $access_log_format,
+    access_log_env_var          => $access_log_env_var,
     error_log_file              => $error_log_file,
     error_log_pipe              => $error_log_pipe,
     error_log_syslog            => $error_log_syslog,
--- spec/classes/placement_wsgi_apache_spec.rb
+++ spec/classes/placement_wsgi_apache_spec.rb
@@ -34,6 +34,7 @@ describe 'placement::wsgi::apache' do
         :access_log_pipe           => nil,
         :access_log_syslog         => nil,
         :access_log_format         => nil,
+        :access_log_env_var        => nil,
         :error_log_file            => nil,
         :error_log_pipe            => nil,
         :error_log_syslog          => nil,
@@ -106,16 +107,18 @@ describe 'placement::wsgi::apache' do
     context 'with custom access logging' do
       let :params do
         {
-          :access_log_format => 'foo',
-          :access_log_syslog => 'syslog:local0',
-          :error_log_syslog  => 'syslog:local1',
+          :access_log_env_var => 'dontlog=1',
+          :access_log_format  => 'foo',
+          :access_log_syslog  => 'syslog:local0',
+          :error_log_syslog   => 'syslog:local1',
         }
       end
 
       it { should contain_openstacklib__wsgi__apache('placement_wsgi').with(
-        :access_log_format => params[:access_log_format],
-        :access_log_syslog => params[:access_log_syslog],
-        :error_log_syslog  => params[:error_log_syslog],
+        :access_log_env_var => params[:access_log_env_var],
+        :access_log_format  => params[:access_log_format],
+        :access_log_syslog  => params[:access_log_syslog],
+        :error_log_syslog   => params[:error_log_syslog],
       )}
     end
 
